0ff30994642b68fa31d2472621621d1381a60a46
Niall Thomson <nthomson@pivotal.io>
2019-07-28 22:48:47 -0700

    Fixed formatting

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ BBL_STATE=/mnt/coder/bbl-state
 wget -O $CODER_BIN_DIR/bosh https://github.com/cloudfoundry/bosh-cli/releases/download/v5.5.1/bosh-cli-5.5.1-linux-amd64
 chmod +x $CODER_BIN_DIR/bosh
 
-sudo apt update && sudo apt install -y build-essential zlibc zlib1g-dev ruby ruby-dev \ 
+sudo apt update && sudo apt install -y build-essential zlibc zlib1g-dev ruby ruby-dev \
     openssl libxslt1-dev libxml2-dev libssl-dev libreadline7 libreadline-dev \
     libyaml-dev libsqlite3-dev sqlite3
 
