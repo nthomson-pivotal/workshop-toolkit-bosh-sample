7ce46dc3168534ff5c6211581f0ffaebb208c043
Niall Thomson <nthomson@pivotal.io>
2019-07-29 18:00:38 -0700

    Temporary fix for destroy

--- destroy.sh
+++ destroy.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+sudo apt update && sudo apt install -y build-essential zlibc zlib1g-dev ruby ruby-dev \
+    openssl libxslt1-dev libxml2-dev libssl-dev libreadline7 libreadline-dev \
+    libyaml-dev libsqlite3-dev sqlite3
+
 env
 
 export PATH=$PATH:/mnt/coder/bin
