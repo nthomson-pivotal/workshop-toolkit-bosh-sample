b9d25f8260f6c4265756d4a20bd5df633a73e995
Niall Thomson <nthomson@pivotal.io>
2019-07-29 18:27:50 -0700

    Temporary fix for destroy

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-mkdir -p ${EXTENSIONS_DIR}/yaml \
-    && curl -JLs --retry 5 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/redhat/vsextensions/vscode-yaml/latest/vspackage | bsdtar --strip-components=1 -xf - -C ${EXTENSIONS_DIR}/yaml extension
+#mkdir -p ${EXTENSIONS_DIR}/yaml \
+#    && curl -JLs --retry 5 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/redhat/vsextensions/vscode-yaml/latest/vspackage | bsdtar --strip-components=1 -xf - -C ${EXTENSIONS_DIR}/yaml extension
